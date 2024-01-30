6f04d2cb79f6aaef332155cd9e74f1b7e8c1516c
Ekaterina Kalashnikova <ladykati73@gmail.com>
2024-01-30 19:46:14 +0300

    6. Правила Checkstyle [#504981 #477259]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,33 +1,33 @@
 package ru.job4j.checkstyle;
 
-public class Brocken {
-    private static final String PRIVET = "";
+public class Broken {
+    private static final String NEW_VALUE = "";
 
     private int sizeOfEmpty = 10;
     private String surname;
     private String name;
 
-    public Brocken() {
+    public Broken() {
 
     }
 
-    void echo() {
+    public void echo() {
 
     }
 
-    void setName(Object obj) {
+    public void setName(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, String b, String c) {
+    public void method(int a, String b, String c) {
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
     }
 
-    void method(int a, int b, int c, int d, int e, int f) {
+    public void method(int a, int b, int c, int d, int e, int f) {
 
     }
 }
