721810be99cd1ea17e7e1c6cc1568ce581c5e515
Ekaterina Kalashnikova <ladykati73@gmail.com>
2024-03-03 12:40:04 +0300

    4. Аттестация [#504878 #484285]

--- src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
@@ -42,13 +42,15 @@ public record AnalyzeByMap() {
     private static void search(List<Pupil> pupils, Map<String, Integer> maps) {
         for (Pupil pupil : pupils) {
             for (Subject sub : pupil.subjects()) {
-                Integer sum = maps.get(sub.name());
+           /*    Integer sum = maps.get(sub.name());
                 if (sum == null) {
                     sum = sub.score();
-                } else {
-                    sum += sub.score();
+               } else {
+                   sum += sub.score();
                 }
-                maps.put(sub.name(), sum);
+/               maps.put(sub.name(), sum);**/
+                Integer sum = maps.getOrDefault(sub.name(), 0);
+                maps.put(sub.name(), sum + sub.score());
             }
         }
     }
